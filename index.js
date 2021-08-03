b84207809cffaefe2f208ad6ad18c7d255d77bff
Eugenijus <augisk@gmail.com>
2021-08-03 18:36:44 +0300

    Pataisymas

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ Jei pirkejas duoda per maza pinigu suma, kasininkas sako: "Nepakanka pinigu!"
 Jei pirkejas duoda per didele pinigu suma, kasininkas sako: "Aciu, jog pirkote! Stai jusu graza - XYZ pinigu!"
 */
 
+// Reikia patiems iskviesti funkcija ir jai duoti tokias reiksmes:
+// a) kaina: 100; kupiuros: [20, 50]
+// b) kaina: 100; kupiuros: [20, 50, 20, 10, 5, 5]
+// c) kaina: 100; kupiuros: [50, 20, 10, 5, 5, 10]
+// Po kiekvieno iskvietimo, privalu isspausdinti kasininko atsakymus.
+
 const kasininkas = require('./kasininkas');
 
 (async () => {
-    // Reikia patiems iskviesti funkcija ir jai duoti tokias reiksmes:
-    // a) kaina: 100; kupiuros: [20, 50]
-    // b) kaina: 100; kupiuros: [20, 50, 20, 10, 5, 5]
-    // c) kaina: 100; kupiuros: [50, 20, 10, 5, 5, 10]
-    // Po kiekvieno iskvietimo, privalu isspausdinti kasininko atsakymus.
+  console.log('');
+
+  const a = await kasininkas(100, [20, 50]);
+  console.log(a);
+
+  const b = await kasininkas(100, [20, 50, 20, 10, 5, 5]);
+  console.log(b);
+
+  const c = await kasininkas(100, [50, 20, 10, 5, 5, 10]);
+  console.log(c);
 })();
--- kasininkas.js
+++ kasininkas.js
@@ -1,5 +1,52 @@
 async function kasininkas(price, cash) {
+  console.log(`Priimtos kupiuros: ${cash.join(', ')}.`);
 
+  let total = 0;
+  for (const number of cash) {
+    total += number;
+  }
+
+  let answer = '';
+  if (total < price) {
+    answer = ' Nepakanka pinigu!\n';
+  } else if (total === price) {
+    answer = 'Aciu, jog pirkote!\n';
+  } else {
+    answer = `Aciu, jog pirkote! Stai jusu graza - ${total - price} pinigu!\n`;
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(answer);
+    }, 2000);
+  });
 }
 
 module.exports = kasininkas;
+
+/*
+async function kasininkas(price, cash) {
+  let receivedCash = [];
+  cashSum = 0;
+  let cashBack = 0;
+  let p = '';
+
+  for (i = 0; i < cash.length; i++) {
+    receivedCash += i !== cash.length - 1 ? `${cash[i]}, ` : `${cash[i]}`;
+    cashSum += cash[i];
+  }
+  cashBack = cashSum - price;
+  if (cashSum < price) {
+    p = 'Nepakanka pinigu!';
+  }
+  if (cashSum === price) {
+    p = 'Aciu, jog pirkote!';
+  }
+  if (cashSum > price) {
+    p = `Aciu, jog pirkote! Stai jusu graza - ${cashBack} pinigu!`;
+  }
+  console.log(`Priimtos kupiuros: ${receivedCash}`);
+}
+
+return new Promise(() => {
+  resolve(p);
+}, 2000);*/
